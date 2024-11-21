c33603d266bdd4af4e638995dc47050ebe8fbab4
Ikeoluwa Oladele <oladelesamson156@gmail.com>
2024-11-20 20:47:09 -0500

    Create login and register page as well as their authorization functionality

---
+++ resources/js/Components/DropDownList.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "DropDownList"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/forms/RegisterationInputField.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "RegisterationInputField"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
